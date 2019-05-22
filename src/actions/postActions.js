15bbffbca9c93d5da64ac46e12c07d6c04f0600d
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-05-22 16:47:28 -0700

    add and remove post created

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,5 +1,10 @@
-const ADD_POST = 'ADD_POST';
+export const ADD_POST = 'ADD_POST';
 export const addPost = body => ({
   type: ADD_POST,
   payload: body
+});
+
+export const REMOVE_POST = 'REMOVE_POST';
+export const removePost = () => ({
+  type: REMOVE_POST
 });
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
+import {
+  ADD_POST,
+  addPost,
+  REMOVE_POST,
+  removePost
+} from './actions/postActions';
 import { createStore } from 'redux';
 
 const initialState = {
-  id: null,
+  title: null,
   body: null
 };
 
 function reducer(state = initialState, action) {
   switch(action.type) {
-    
+    case ADD_POST:
+      return { ...state, body: action.payload };
+    case REMOVE_POST:
+      return { ...state, body: null };
+    default:
+      return state;
   }
 }
 
+const store = createStore(reducer);
 
-const store = createStore();
+store.dispatch(addPost('hi im colin'));
+store.dispatch(removePost());
+
+console.log(store.getState());
