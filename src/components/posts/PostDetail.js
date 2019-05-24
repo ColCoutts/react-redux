a1b0e9a228e0b9f2c73a7eafd3a585677cd9f23f
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-05-23 17:50:54 -0700

    added comments

---
+++ src/components/posts/PostDetail.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CommentsByPostId from '../../containers/comments/CommentsByPostId';
+import CreateComment from '../../containers/comments/CreateComment';
+
+function PostDetail({ post }) {
+  
+}
